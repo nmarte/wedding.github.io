2b5925b89361e0ea08151715d66186cf49ededc9
nmarte <nmarte@ivytech.edu>
2025-11-19 12:18:35 -0500

    sync+html+css

---
+++ main/main.js
@@ -0,0 +1,12 @@
+const gra = function(min,max) {
+    return Math.random()*(max-min)+min;
+}
+const init = function(){
+    let items =
+document.querySelectorAll('section');
+    for (let i = 0; i < items.length; i++){
+        items[i].style.background = randomColor({luminosity:'dark'});
+    }
+    cssScrollSnapPolyfill()
+}
+init();
